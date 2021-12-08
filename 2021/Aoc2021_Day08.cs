692aa6c97be6fbc11addb3ce725694e95b4eabe8
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2021-12-08 20:13:52 +0100

    day 8a+b working

--- 2021/Aoc2021_Day08.cs
+++ 2021/Aoc2021_Day08.cs
@@ -23,60 +23,70 @@ public class Aoc2021_Day08 : BaseDay
         var outPuts = Input.Select(x => x.Split('|')[1].Trim(' ')).ToList();
 
         long sum = 0;
-        // foreach (var line in digitSets)
         for (int line = 0; line < digitSets.Count(); line++)
         {
-            // var digitSet = line.Split(' ').ToList();
             var digitSet = digitSets[line].Split(' ').ToList();
             var output = outPuts[line].Split(' ').ToList();
             Debug.Assert(digitSet.Count() == 10);
 
             var digitDictionary = RemoveEasyDigits(digitSet);
-
-            var three = digitSet.First(x => x.Length == 5 && x.Intersect(digitDictionary[1]).Count() == 2);
-            digitDictionary.Add(3, three);
-            digitSet.Remove(three);
-
-            var nine = digitSet.Single(x => x.Length == 6 && x.Intersect(digitDictionary[4]).Count() == 4);
-            digitDictionary.Add(9, nine);
-            digitSet.Remove(nine);
-
-            var zero = digitSet.Single(x => x.Length == 6 && x.Intersect(digitDictionary[1]).Count() == 2);
-            digitDictionary.Add(0, zero);
-            digitSet.Remove(zero);
-
-            var six = digitSet.Single(x => x.Length == 6);
-            digitDictionary.Add(6, six);
-            digitSet.Remove(six);
-
-            var five = digitSet.Single(x => x.Length == 5 && x.Intersect(digitDictionary[6]).Count() == 5);
-            digitDictionary.Add(5, five);
-            digitSet.Remove(five);
-
-            var two = digitSet.Single();
-            digitDictionary.Add(2, two);
-            digitSet.Remove(two);
+            RemoveHardDigits(digitSet, digitDictionary);
 
             Debug.Assert(digitSet.Count() == 0);
-            
-            // Calc sum
-            var frasse = new HashSet<string>();
-            "frasse".ToHashSet();
-        }
 
+            // Decode output and calculate sum
+            string decoded = "";
+            foreach (var code in output)
+            {
+                var digit = digitDictionary.Single(x => x.Value.ToHashSet().SetEquals(code.ToHashSet())).Key;
+                decoded += digit.ToString();
+            }
+            int decodedSum = int.Parse(decoded);
+            sum += decodedSum;
+        }
+        System.Console.WriteLine($"Sum is {sum}");
     }
 
-    private static Dictionary<int, string> RemoveEasyDigits(List<string> digitSet)
+    // Method intentionally modfies parameters
+    private static void RemoveHardDigits(List<string> digitSet, Dictionary<int, HashSet<char>> digitDictionary)
     {
-        var digitDictionary = new Dictionary<int, string>();
+        var three = digitSet.First(x => x.Length == 5 && x.Intersect(digitDictionary[1]).Count() == 2);
+        digitDictionary.Add(3, three.ToHashSet());
+        digitSet.Remove(three);
+
+        var nine = digitSet.Single(x => x.Length == 6 && x.Intersect(digitDictionary[4]).Count() == 4);
+        digitDictionary.Add(9, nine.ToHashSet());
+        digitSet.Remove(nine);
+
+        var zero = digitSet.Single(x => x.Length == 6 && x.Intersect(digitDictionary[1]).Count() == 2);
+        digitDictionary.Add(0, zero.ToHashSet());
+        digitSet.Remove(zero);
+
+        var six = digitSet.Single(x => x.Length == 6);
+        digitDictionary.Add(6, six.ToHashSet());
+        digitSet.Remove(six);
+
+        var five = digitSet.Single(x => x.Length == 5 && x.Intersect(digitDictionary[6]).Count() == 5);
+        digitDictionary.Add(5, five.ToHashSet());
+        digitSet.Remove(five);
+
+        var two = digitSet.Single();
+        digitDictionary.Add(2, two.ToHashSet());
+        digitSet.Remove(two);
+    }
+
+    // Method intentionally modfies parameter
+    private static Dictionary<int, HashSet<char>> RemoveEasyDigits(List<string> digitSet)
+    {
+        var digitDictionary = new Dictionary<int, HashSet<char>>();
         var one = digitSet.Single(x => x.Length == 2);
         var four = digitSet.Single(x => x.Length == 4);
         var seven = digitSet.Single(x => x.Length == 3);
         var eight = digitSet.Single(x => x.Length == 7);
-        digitDictionary.Add(1, one);
-        digitDictionary.Add(4, four);
-        digitDictionary.Add(7, seven);
-        digitDictionary.Add(8, eight);
+        digitDictionary.Add(1, one.ToHashSet());
+        digitDictionary.Add(4, four.ToHashSet());
+        digitDictionary.Add(7, seven.ToHashSet());
+        digitDictionary.Add(8, eight.ToHashSet());
         digitSet.Remove(one);
         digitSet.Remove(four);
         digitSet.Remove(seven);
