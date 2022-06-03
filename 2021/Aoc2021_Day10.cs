29697de09fac525446a50e3756b75e82891ef077
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-06-03 22:17:05 +0200

    day 10 b + tests

--- 2021/Aoc2021_Day10.cs
+++ 2021/Aoc2021_Day10.cs
@@ -1,6 +1,11 @@
 public class Aoc2021_Day10 : BaseDay
 {
-    private Dictionary<char, char> Matching = new();
+    private Dictionary<char, char> Matching = new(){
+        {'(', ')'},
+        {'{', '}'},
+        {'[', ']'},
+        {'<', '>'}
+    };
 
     private List<string> RemoveThese = new List<string>() {
         "()",
@@ -16,13 +21,15 @@ public class Aoc2021_Day10 : BaseDay
         {'>', 25137}
     };
 
+    private Dictionary<char, int> CompletionScore = new() {
+        {')', 1},
+        {']', 2},
+        {'}', 3},
+        {'>', 4}
+    };
+
     public Aoc2021_Day10(string inputFileName) : base(inputFileName)
-    {
-        Matching.Add('(', ')');
-        Matching.Add('{', '}');
-        Matching.Add('[', ']');
-        Matching.Add('<', '>');
-    }
+    { }
 
     public override void RunA()
     {
@@ -44,7 +51,24 @@ public class Aoc2021_Day10 : BaseDay
         System.Console.WriteLine($"Total score is {totalScore}");
     }
 
-    public string FindFirstIllegal(string line)
+    internal string FindFirstIllegal(string line)
+    {
+        string lineFiltered = FilterClosedExprs(line);
+
+        for (int i = 0; i < lineFiltered.Length - 1; i++)
+        {
+            if (Matching.ContainsKey(lineFiltered[i]) &&
+                Matching.ContainsValue(lineFiltered[i + 1]) &&
+                Matching[lineFiltered[i]] != lineFiltered[i + 1])
+            {
+                return lineFiltered[i + 1].ToString();
+            }
+        }
+
+        return "";
+    }
+
+    private string FilterClosedExprs(string line)
     {
         string lineFiltered = new string(line);
         int oldLength = 0;
@@ -60,21 +84,57 @@ public class Aoc2021_Day10 : BaseDay
             newLength = lineFiltered.Length;
         }
 
-        for (int i = 0; i < lineFiltered.Length - 1; i++)
-        {
-            if (Matching.ContainsKey(lineFiltered[i]) &&
-                Matching.ContainsValue(lineFiltered[i + 1]) &&
-                Matching[lineFiltered[i]] != lineFiltered[i + 1])
-            {
-                return lineFiltered[i + 1].ToString();
-            }
-        }
-
-        return "";
+        return lineFiltered;
     }
 
     public override void RunB()
     {
+        var incompleteLines = GetIncompleteLines(Input);
+        List<long> CompletionScores = new();
+        foreach (var line in incompleteLines)
+        {
+            var filtered = FilterClosedExprs(line);
+            var completeWith = GetAutocompletion(filtered);
+            CompletionScores.Add(GetCompletionScore(completeWith));
+        }
 
+        CompletionScores.Sort();
+
+        var middleScoreIndex = CompletionScores.Count / 2;
+        System.Console.WriteLine($"Score is {CompletionScores[middleScoreIndex]}");
+
+    }
+
+    internal long GetCompletionScore(string completion)
+    {
+        long score = 0;
+        long factor = 5;
+
+        foreach (var character in completion)
+        {
+            score = score * factor;
+            score = score + CompletionScore[character];
+        }
+        return score;
+    }
+
+    internal string GetAutocompletion(string line)
+    {
+        var completeWith = string.Concat(line.Reverse().Select(x => Matching[x]));
+        return completeWith;
+    }
+
+    internal List<string> GetIncompleteLines(List<string> inputLines)
+    {
+        List<string> incompleteLines = new();
+        // Discard corrupted lines
+
+        foreach (var line in inputLines)
+        {
+            string firstIncorrect = FindFirstIllegal(line);
+            if (string.IsNullOrEmpty(firstIncorrect))
+                incompleteLines.Add(line);
+        }
+        return incompleteLines;
     }
 }
--- tests/2021/Aoc2021_Day10_Tests.cs
+++ tests/2021/Aoc2021_Day10_Tests.cs
@@ -21,4 +21,35 @@ public class Aoc2021_Day10_Tests
 
         Assert.Equal(expectedOutput, firstUnmatched);
     }
+
+    [Theory]
+    [InlineData("{([(<{}[<>[]}>{[]{[(<()>", 0)]
+    [InlineData("[[<[([]))<([[{}[[()]]]", 0)]
+    [InlineData("[({(<(())[]>[[{[]{<()<>>", 1)]
+    [InlineData("<{([{{}}[<[[[<>{}]]]>[]]", 1)]
+    public void Only_Incomplete_Lines_returned(string input, int lineCount)
+    {
+        var lines = aoc2021_Day10.GetIncompleteLines(new List<string>() { input });
+
+        Assert.Equal(lineCount, lines.Count);
+    }
+
+    [Theory]
+    [InlineData("[({([[{{", "}}]])})]")]
+    public void Autocomplete_completes_line(string input, string output)
+    {
+        var autocompleted = aoc2021_Day10.GetAutocompletion(input);
+
+        Assert.Equal(output, autocompleted);
+    }
+
+    [Theory]
+    [InlineData("}}]])})]", 288957)]
+    [InlineData(")}>]})", 5566)]
+    public void CompletionScore_calculated_correctly(string completion, int score)
+    {
+        var calculatedScore = aoc2021_Day10.GetCompletionScore(completion);
+
+        Assert.Equal(score, calculatedScore);
+    }
 }
