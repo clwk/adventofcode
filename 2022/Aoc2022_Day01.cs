dca0891f2095d885014bf70d46e760ece8feba6e
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-01 10:39:39 +0100

    input

---
+++ 2022/Aoc2022_Day01.cs
@@ -0,0 +1,15 @@
+public class Aoc2022_Day01 : BaseDay
+{
+    public Aoc2022_Day01(string inputFileName) : base(inputFileName)
+    {  }
+
+    public override void RunA()
+    {
+    
+    }
+
+    public override void RunB()
+    {
+        
+    }
+}
