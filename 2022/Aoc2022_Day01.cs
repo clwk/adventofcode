20d1e564aac88f7d048ef72be9663bdad2687f3d
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-01 11:11:58 +0100

    2022 day 1 a and b working

--- 2022/Aoc2022_Day01.cs
+++ 2022/Aoc2022_Day01.cs
@@ -1,15 +1,34 @@
 public class Aoc2022_Day01 : BaseDay
 {
     public Aoc2022_Day01(string inputFileName) : base(inputFileName)
-    {  }
+    { }
+
+    private List<int> ResultFromA { get; set; }
 
     public override void RunA()
     {
-    
+        var caloriesByElf = new List<int>();
+        int caloriesSum = 0;
+        foreach (var line in Input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                caloriesByElf.Add(caloriesSum);
+                caloriesSum = 0;
+            }
+            else
+            {
+                caloriesSum += int.Parse(line);
+            }
+        }
+        ResultFromA = caloriesByElf;
+        System.Console.WriteLine($"Result from A {caloriesByElf.Max()}");
+
     }
 
     public override void RunB()
     {
-        
+        var resultFromB = ResultFromA.OrderByDescending(x => x).Take(3).Sum();
+        System.Console.WriteLine($"Result from B {resultFromB}");
     }
 }
