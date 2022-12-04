8c3fe0e0a387461f6d9215fd17872357c18830f2
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-04 12:24:53 +0100

    2022 day 03

--- 2022/Aoc2022_Day03.cs
+++ 2022/Aoc2022_Day03.cs
@@ -1,7 +1,7 @@
 public class Aoc2022_Day03 : BaseDay
 {
     public Aoc2022_Day03(string inputFileName) : base(inputFileName)
-    {  }
+    { }
 
     public override void RunA()
     {
@@ -30,6 +30,21 @@ public class Aoc2022_Day03 : BaseDay
 
     public override void RunB()
     {
-        
+        var commonPrios = GetCommonPrios();
+        var sum = commonPrios.Select(GetPriority).Sum();
+        Console.WriteLine($"Sum is {sum}");
+    }
+
+    private List<char> GetCommonPrios()
+    {
+        List<char> commonPrios = new();
+        for (int i = 0; i < Input.Count() / 3; i++)
+        {
+            var lines3 = Input.Skip(i * 3).Take(3).ToList();
+            var common = lines3[0].Intersect(lines3[1]).Intersect(lines3[2]).First();
+            commonPrios.Add(common);
+        }
+
+        return commonPrios;
     }
 }
