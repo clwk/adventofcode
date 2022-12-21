b99495b77638721c5a329c7705521f50f58d7786
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-21 21:10:19 +0100

    day 21 empty

---
+++ 2022/Aoc2022_Day21.cs
@@ -0,0 +1,15 @@
+public class Aoc2022_Day21 : BaseDay
+{
+    public Aoc2022_Day21(string inputFileName) : base(inputFileName)
+    {  }
+
+    public override void RunA()
+    {
+    
+    }
+
+    public override void RunB()
+    {
+        
+    }
+}
