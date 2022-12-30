7fcce8cb8f57514b8a890250ca64d33de81c4a3d
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-30 18:48:27 +0100

    refactor part a

--- 2022/Aoc2022_Day15.cs
+++ 2022/Aoc2022_Day15.cs
@@ -7,28 +7,35 @@ public class Aoc2022_Day15 : BaseDay
 
     private List<(int x, int y)> _sensors = new();
     private List<(int x, int y)> _beacons = new();
-    private HashSet<int> NoBeaconCols = new();
+    private Dictionary<int, HashSet<int>> NoBeaconColsB = new();
     private const int rowToCheck = 2000000;
 
     public override void RunA()
     {
+        var cols = GetNoBeaconCols(rowToCheck);
+        System.Console.WriteLine($"Number of cols: {cols.Count}");
+    }
+
+    private HashSet<int> GetNoBeaconCols(int checkRow)
+    {
+        HashSet<int> noBeaconCols = new();
         for (int i = 0; i < _sensors.Count; i++)
         {
             var searchDistance = GetDistance(_sensors[i], _beacons[i]);
             var sensorRow = _sensors[i].y;
             var sensorCol = _sensors[i].x;
 
-            var verticalDistance = Math.Abs(sensorRow - rowToCheck);
+            var verticalDistance = Math.Abs(sensorRow - checkRow);
             if (searchDistance > verticalDistance)
             {
                 var fillCols = searchDistance - verticalDistance;
                 for (int col = sensorCol - fillCols; col < sensorCol + fillCols; col++)
                 {
-                    NoBeaconCols.Add(col);
+                    noBeaconCols.Add(col);
                 }
             }
         }
-        System.Console.WriteLine($"Number of cols: {NoBeaconCols.Count}");
+        return noBeaconCols;
     }
 
     private void ParseInput()
@@ -45,6 +52,6 @@ public class Aoc2022_Day15 : BaseDay
 
     public override void RunB()
     {
-
+        // NoBeaconColsB.TryGetValue()
     }
 }
