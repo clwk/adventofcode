1f62731ffcff1d37375c388455d39313196f16f5
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-12-31 16:00:48 +0100

    intersections on b. troubleshooting gnobeaconcols

--- 2022/Aoc2022_Day15.cs
+++ 2022/Aoc2022_Day15.cs
@@ -52,35 +52,89 @@ public class Aoc2022_Day15 : BaseDay
         return Math.Abs(sensor.x - beacon.x) + Math.Abs(sensor.y - beacon.y);
     }
 
+    // public override void RunB()
+    // {
+    //     const int maxRow = 4000000;
+    //     // Find ranges to not check
+    //     // var initialRows = Enumerable.Range(0, maxRow);
+    //     // var allRowsNoCheck = GetAllRowsNoCheck();
+
+    //     // var rowsToCheck = initialRows.Except(allRowsNoCheck).ToList();
+
+    //     (int x, int y) distressPos = (0, 0);
+    //     // int row = 0;
+    //     var rowsToCheck = _sensors.Select(p => p.y);
+
+    //     // while (distressPos == (0, 0) && row <= maxRow)
+    //     // foreach (var row in rowsToCheck)
+    //     for (int row = _sensors.MinBy(p => p.y).y; row < _sensors.MaxBy(p => p.y).y; row++)
+    //     {
+    //         var colsWithNoBeacon = GetNoBeaconCols(row);
+    //         // var colsSorted1 = colsWithNoBeacon.OrderByDescending(x => x);
+    //         // var colsSorted2 = colsWithNoBeacon.OrderBy(x => x);
+    //         var possibleCols = Enumerable.Range(1, maxRow).Except(colsWithNoBeacon);
+    //         if (possibleCols.Count() == 1)
+    //             distressPos = (possibleCols.Single(), row);
+    //         System.Console.WriteLine($"Row {row}, {possibleCols.Count()} possible beacons");
+    //         // row++;
+    //     }
+
+    //     var tuningFrequency = 4000000 * distressPos.x + distressPos.y;
+    //     System.Console.WriteLine($"Tuning frequency {tuningFrequency}");
+    // }
+
     public override void RunB()
     {
-        const int maxRow = 4000000;
-        // Find ranges to not check
-        // var initialRows = Enumerable.Range(0, maxRow);
-        // var allRowsNoCheck = GetAllRowsNoCheck();
+        const int maxRow = 20;
+        var intersections = GetIntersections();
+        var searchRows = Enumerable.Range(0, maxRow);
+        var possibleRows = searchRows.Except(intersections.Select(p => p.y));
 
-        // var rowsToCheck = initialRows.Except(allRowsNoCheck).ToList();
+        var testSort = intersections.OrderBy(p => p.y).ToList();
+        var startAt = testSort.ElementAt(testSort.Count / 2);
 
         (int x, int y) distressPos = (0, 0);
-        // int row = 0;
-        var rowsToCheck = _sensors.Select(p => p.y);
-
-        // while (distressPos == (0, 0) && row <= maxRow)
-        // foreach (var row in rowsToCheck)
-        for (int row = _sensors.MinBy(p => p.y).y; row < _sensors.MaxBy(p => p.y).y; row++)
+        int row = startAt.y;
+        while (distressPos == (0, 0) && row <= maxRow)
         {
             var colsWithNoBeacon = GetNoBeaconCols(row);
-            // var colsSorted1 = colsWithNoBeacon.OrderByDescending(x => x);
-            // var colsSorted2 = colsWithNoBeacon.OrderBy(x => x);
+            //         // var colsSorted1 = colsWithNoBeacon.OrderByDescending(x => x);
+            //         // var colsSorted2 = colsWithNoBeacon.OrderBy(x => x);
             var possibleCols = Enumerable.Range(1, maxRow).Except(colsWithNoBeacon);
             if (possibleCols.Count() == 1)
                 distressPos = (possibleCols.Single(), row);
             System.Console.WriteLine($"Row {row}, {possibleCols.Count()} possible beacons");
-            // row++;
+            row++;
         }
 
         var tuningFrequency = 4000000 * distressPos.x + distressPos.y;
         System.Console.WriteLine($"Tuning frequency {tuningFrequency}");
+    }
+
+    private List<(int x, int y)> GetIntersections()
+    {
+        List<List<(int x, int y)>> listOfRhombuses = new();
+
+        for (int i = 0; i < _sensors.Count; i++)
+        {
+            var rhombus1 = GetRhombus(_sensors[i], _beacons[i]);
+            listOfRhombuses.Add(rhombus1);
+        }
+
+        List<(int x, int y)> intersections = new();
+        foreach (var rhombus1 in listOfRhombuses)
+        {
+            foreach (var rhombus2 in listOfRhombuses)
+            {
+                // Should not compare when rhombuses are the same
+                if (rhombus1 == rhombus2)
+                    continue;
+                var intersection = rhombus1.Intersect(rhombus2).ToList();
+                intersections.AddRange(intersection);
+            }
+        }
+        var uniqueIntersections = intersections.Distinct().ToList();
+        return uniqueIntersections;
     }
 
     private HashSet<int> GetAllRowsNoCheck()
@@ -107,16 +161,23 @@ public class Aoc2022_Day15 : BaseDay
         var distance = GetDistance(sensor, beacon);
 
         (int x, int y) line1Start = (sensor.x - distance, sensor.y);
-        pointsOnRhombus.AddRange(GetPointsOnLine1or3(line1Start, distance));
+        var pointsOnLine1 = GetPointsOnLine1or3(line1Start, distance);
+        pointsOnRhombus.AddRange(pointsOnLine1);
 
         // line2
         (int x, int y) line2Start = (sensor.x, sensor.y - distance);
+        var pointsOnLine2 = GetPointsOnLine2or4(line2Start, distance);
+        pointsOnRhombus.AddRange(pointsOnLine2);
 
         // line3
         (int x, int y) line3Start = (sensor.x, sensor.y + distance);
+        var pointsOnLine3 = GetPointsOnLine1or3(line3Start, distance);
+        pointsOnRhombus.AddRange(pointsOnLine3);
 
         // line4
         (int x, int y) line4Start = (sensor.x - distance, sensor.y);
+        var pointsOnLine4 = GetPointsOnLine2or4(line4Start, distance);
+        pointsOnRhombus.AddRange(pointsOnLine4);
 
         var points = pointsOnRhombus.Distinct().ToList();
         return points;
@@ -124,16 +185,16 @@ public class Aoc2022_Day15 : BaseDay
 
     private static IEnumerable<(int x, int y)> GetPointsOnLine1or3((int x, int y) lineStart, int distance)
     {
-        var lineX = Enumerable.Range(lineStart.x, distance);
-        var lineY = Enumerable.Range(lineStart.y - distance, distance).Reverse();
+        var lineX = Enumerable.Range(lineStart.x, distance + 1);
+        var lineY = Enumerable.Range(lineStart.y - distance, distance + 1).Reverse();
         var points = lineX.Zip(lineY, (x, y) => (x, y));
         return points;
     }
 
     private static IEnumerable<(int x, int y)> GetPointsOnLine2or4((int x, int y) lineStart, int distance)
     {
-        var lineX = Enumerable.Range(lineStart.x, distance);
-        var lineY = Enumerable.Range(lineStart.y, distance);
+        var lineX = Enumerable.Range(lineStart.x, distance + 1);
+        var lineY = Enumerable.Range(lineStart.y, distance + 1);
         var points = lineX.Zip(lineY, (x, y) => (x, y));
         return points;
     }
